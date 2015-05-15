2eaca98b6518e1eaadf6122c89d2b227d0deb39a
Gastón I. Silva <givanse@gmail.com>
2015-05-15 10:30:15 -0500

    install sane-auth used fix for https://github.com/ember-cli/ember-cli/issues/4085

--- client/app/router.js
+++ client/app/router.js
@@ -6,6 +6,12 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('user', function() {});
+
+  this.route("login");
+  this.route("register");
 });
+
+
 
 export default Router;
